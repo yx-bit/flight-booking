a5bfece4d98c86b3e91005b0adfadb0778d68817
yongxiang-chen <yongxiang.chen@budweiserapac.com>
2025-04-06 19:49:34 +0800

    1.添加MCP的server的tools自动化注册示例

--- mcp-server/src/main/java/com/bit/mcpserver/TestToolService.java
+++ mcp-server/src/main/java/com/bit/mcpserver/TestToolService.java
@@ -1,11 +1,14 @@
 package com.bit.mcpserver;
 
 import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestToolService {
 
+    @Autowired
+    private OpenMeteoService openMeteoService;
     TestToolService(){
         System.out.println("初始化");
     }
@@ -13,5 +16,11 @@ public class TestToolService {
     public String testTool(int a,int b)
     {
         return a+b+"";
+    }
+
+    @Tool(description = "获取指定经纬度的天气预报")
+    public String getWeather(double latitude, double longitude)
+    {
+        return openMeteoService.getWeatherForecastByLocation(latitude,longitude);
     }
 }
