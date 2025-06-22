d1d7e890ba6eeea928c00378b058d8f7415a22c1
yongxiang-chen <yongxiang.chen@budweiserapac.com>
2025-06-22 12:49:55 +0800

    feat(playground): 添加聊天流功能并增强可观察性 - 在 AssistantController 中添加了 chat/stream端点，实现聊天流功能 - 更新了 CustomerSupportAssistant 中的提示信息，使用"工具"替代"函数" - 在 pom.xml 中添加了 spring-ai-starter-mcp-server-webflux 和 Zipkin 相关依赖 - 更新了 application.properties，增加了可观测性相关配置

--- src/main/java/com/bit/flightbooking/playground/client/AssistantController.java
+++ src/main/java/com/bit/flightbooking/playground/client/AssistantController.java
@@ -4,6 +4,10 @@ import com.alibaba.cloud.ai.graph.CompiledGraph;
 import com.alibaba.cloud.ai.graph.StateGraph;
 import com.alibaba.cloud.ai.graph.exception.GraphStateException;
 import com.bit.flightbooking.playground.services.CustomerSupportAssistant;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +25,14 @@ public class AssistantController {
 	private final CustomerSupportAssistant agent;
 	private final CompiledGraph compiledGraph;
 
-	public AssistantController(CustomerSupportAssistant agent, StateGraph stateGraph) throws GraphStateException {
+	private final ChatModel chatModel;
+
+	private final ToolCallbackProvider[] toolCallbackProvider;
+	public AssistantController(CustomerSupportAssistant agent, StateGraph stateGraph, ChatModel chatModel, ToolCallbackProvider[] toolCallbackProvider) throws GraphStateException {
 		this.agent = agent;
         this.compiledGraph = stateGraph.compile();
+        this.chatModel = chatModel;
+        this.toolCallbackProvider = toolCallbackProvider;
     }
 
 	@RequestMapping(path="/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
@@ -35,5 +44,9 @@ public class AssistantController {
 	public String simpleChat(@RequestParam(name = "query") String query) throws Exception {
 
 		return compiledGraph.invoke(Map.of("input", query)).get().value("solution").get().toString();
+	}
+	@GetMapping("/chat/stream")
+	public Flux<String> simpleChatStream(@RequestParam(name = "query") String query) throws Exception {
+		return ChatClient.builder(chatModel).defaultAdvisors(new SimpleLoggerAdvisor()).defaultToolCallbacks(toolCallbackProvider).build().prompt().user(query).stream().content();
 	}
 }
